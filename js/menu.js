d951b7b1d46cb3e9b60f9fcc150b61697ee54052
Takahiro Oku <pigment@mac.com>
2024-02-25 17:09:01 +0900

    画面分けをした

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+document.getElementById('menu').innerHTML = 
+    "        <nav class='navbar navbar-expand-sm bg-light'>" +
+    "<div class='container-fluid'>" +
+    "    <ul class='navbar-nav'>" +
+    "        <li class='nav-item'>" +
+    "            <a class='nav-link' href='index.html'>ゲーム画面</a>" +
+    "        </li>" +
+    "        <li class='nav-item'>" +
+    "            <a class='nav-link' href='manage.html'>開発画面</a>" +
+    "        </li>" +
+    "        <li class='nav-item'>" +
+    "            <a class='nav-link' href='#'>Link 3</a>" +
+    "        </li>" +
+    "    </ul>" +
+    "</div>" +
+    "</nav>";
