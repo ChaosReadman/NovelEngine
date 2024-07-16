dcd4134cae893b5d4886f211f33833034cbfb6d9
ChaosReadman <pigment@mac.com>
2024-07-16 13:05:30 +0900

    朝会でパーティクルの載せ替えをした

--- js/Perticle.js
+++ js/Perticle.js
@@ -1,13 +1,31 @@
 var jsPerticle = {
   "frames": [
     {
-      "filename": "Perticle (Particle) #Idle.aseprite",
-      "frame": { "x": 0, "y": 0, "w": 12, "h": 12 },
+      "filename": "Perticle (Particle) #Idle 0.aseprite",
+      "frame": { "x": 32, "y": 25, "w": 19, "h": 19 },
       "rotated": false,
       "trimmed": true,
-      "spriteSourceSize": { "x": 10, "y": 10, "w": 12, "h": 12 },
+      "spriteSourceSize": { "x": 7, "y": 8, "w": 19, "h": 19 },
       "sourceSize": { "w": 32, "h": 32 },
-      "duration": 500
+      "duration": 100
+    },
+    {
+      "filename": "Perticle (Particle) #Idle 1.aseprite",
+      "frame": { "x": 32, "y": 0, "w": 23, "h": 25 },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": { "x": 5, "y": 4, "w": 23, "h": 25 },
+      "sourceSize": { "w": 32, "h": 32 },
+      "duration": 100
+    },
+    {
+      "filename": "Perticle (Particle) #Idle 2.aseprite",
+      "frame": { "x": 0, "y": 0, "w": 32, "h": 32 },
+      "rotated": false,
+      "trimmed": false,
+      "spriteSourceSize": { "x": 0, "y": 0, "w": 32, "h": 32 },
+      "sourceSize": { "w": 32, "h": 32 },
+      "duration": 100
     }
   ],
   "meta": {
@@ -15,10 +33,10 @@ var jsPerticle = {
     "version": "1.3.7-x64",
     "image": "Perticle.png",
     "format": "RGBA8888",
-    "size": { "w": 12, "h": 12 },
+    "size": { "w": 55, "h": 44 },
     "scale": "1",
     "frameTags": [
-      { "name": "Idle", "from": 0, "to": 0, "direction": "forward", "color": "#000000ff" }
+      { "name": "Idle", "from": 0, "to": 2, "direction": "forward", "color": "#000000ff" }
     ],
     "layers": [
       { "name": "Particle", "opacity": 255, "blendMode": "normal" }
